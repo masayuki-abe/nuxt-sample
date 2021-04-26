07dd2ee7cd7f39e28f9a5057ae15ab0202b7f701
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-04-26 14:53:00 +0900

    HTMLとcssを記述し、HtmlHeader.vueでhamburger.vueを読み込むよう設定

---
+++ components/atoms/buttons/hamburger.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="btn-hamburger">
+    <span />
+    <span />
+    <span />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.btn-hamburger{
+  position: absolute;
+  width: 10%;
+  span{
+    @include dis(inline-block);
+
+    position: absolute;
+    left: 0;
+    top: 50%;
+    width: 100%;
+    height: 3px;
+    background-color: $dark;
+    transition: margin .2s ease, opacity .1s ease .2s, transform .4s ease .2s;
+
+    &:nth-child(1){
+      margin-top: -10px;
+    }
+    &:nth-child(3){
+      margin-top: 10px;
+    }
+  }
+}
+</style>
--- components/organisms/HtmlHeader.vue
+++ components/organisms/HtmlHeader.vue
@@ -1,5 +1,16 @@
 <template>
   <header id="header">
     <h1>Header</h1>
+    <BtnHamburger />
   </header>
 </template>
+
+<script>
+import BtnHamburger from '@/components/atoms/buttons/hamburger'
+
+export default {
+  components: {
+    BtnHamburger
+  }
+}
+</script>
