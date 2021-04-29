5daac4c714b00f635c575c1b3626c3bd0b030bae
masayuki-abe <cafenorthfox@gmail.com>
2021-04-29 15:05:52 +0900

    モーダルウィンドウ用のコンポネントを作成

---
+++ components/atoms/etc/Modal.vue
@@ -0,0 +1,11 @@
+<template>
+  <transition name="modal">
+    <div class="modal__base">
+      <div class="modal__wrap">
+        <div class="modal__content">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
