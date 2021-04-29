d5567f91cd1161d2afe08d7c0a96a551d706317c
masayuki-abe <cafenorthfox@gmail.com>
2021-04-29 16:15:01 +0900

    モーダルウィンドウを開くためのボタンとslotを記述

--- components/templates/FrontPage.vue
+++ components/templates/FrontPage.vue
@@ -1,5 +1,40 @@
 <template>
   <main>
     <p>FrontPage</p>
+
+    <div class="modal-window">
+      <button @click="openModal">
+        Open the 'Modal Window'
+      </button>
+      <Modal v-if="modalFlag">
+        <p>test</p>
+        <p>testtesttest</p>
+      </Modal>
+      <button @click="closeModal">
+        Close
+      </button>
+    </div><!--/ modal window-->
   </main>
 </template>
+
+<script>
+import Modal from '@/components/atoms/etc/Modal'
+export default {
+  components: {
+    Modal
+  },
+  data () {
+    return {
+      modalFlag: false
+    }
+  },
+  methods: {
+    openModal () {
+      this.modalFlag = true
+    },
+    closeModal () {
+      this.modalFlag = false
+    }
+  }
+}
+</script>
