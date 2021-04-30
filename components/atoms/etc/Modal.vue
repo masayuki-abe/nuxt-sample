1ce4f961d5943a4bcb4e472ac627823ae384fd95
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-04-30 11:12:08 +0900

    モーダルウィンドウをcssで修飾

--- components/atoms/etc/Modal.vue
+++ components/atoms/etc/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition name="modal">
+  <transition name="modal" appear>
     <div class="modal__base">
       <div class="modal__wrap">
         <div class="modal__content">
@@ -9,3 +9,62 @@
     </div>
   </transition>
 </template>
+
+<style lang="scss">
+.modal{
+  &__base{
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 1000;
+    width: 100%;
+    height: 100%;
+    background-color: rgba($dark, 0.7);
+  }
+
+  &__wrap{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    width: 90%;
+    height: 80%;
+    transform: translate(-50%, -50%);
+  }
+
+  &__content{
+    @include fontSet(32, 48, 100, $tab);
+
+    overflow-y: scroll;
+    width: 100%;
+    height: 100%;
+    padding: 1em;
+    background-color: $white;
+    border: 2px $black solid;
+  }
+}
+
+//transition
+.modal{
+  &-enter-active,
+  &-leave-active{
+    transition: opacity 0.6s ease;
+
+    .modal__content{
+      transition: opacity 0.4s ease, transform 0.8s ease;
+    }
+  }
+  &-leave-active{
+    transition: opacity 0.6s ease 0.4s;
+  }
+
+  &-enter,
+  &-leave-to{
+    opacity: 0;
+
+    .modal__content{
+      opacity: 0;
+      transform: translateY(-20px);
+    }
+  }
+}
+</style>
