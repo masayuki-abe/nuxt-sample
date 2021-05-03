5ce02d36d9c4cc99cd99d7ded71c6602e83796b9
MasayukiAbe <abe@weboss.jp>
2021-05-03 15:49:47 +0900

    サンプルページの配列初期設定

---
+++ components/molecules/lists/SampleList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="sample-list">
+    <li v-for="sample in samples" :key="sample.id">
+      {{ sample.id }}{{ sample.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  computed: {
+    samples () {
+      const sampleList = [
+        {
+          id: '1',
+          hash: 'hamburger-menu',
+          name: 'Hamburger Menu',
+          nameJa: 'ハンバーガーメニュー'
+        },
+        {
+          id: '2',
+          hash: 'modal-window',
+          name: 'Modal Window',
+          nameJa: 'モーダルウィンドウ'
+        }
+      ]
+      return sampleList
+    }
+  }
+}
+</script>
--- components/templates/SampleList.vue
+++ components/templates/SampleList.vue
@@ -2,15 +2,13 @@
   <main>
     <section id="sample-list__box01" class="common-box">
       <h2>Sample List</h2>
-      <p v-for="(samples, index) in sampleLists" :key="samples.id" class="common-txt">
-        {{samples.name}}
-      </p>
+      <sampleLists />
     </section>
   </main>
 </template>
 
 <script>
-import sampleLists from '@/assets/js/sampleList.js'
+import sampleLists from '@/components/molecules/lists/sampleList'
 
 export default {
   components: {
