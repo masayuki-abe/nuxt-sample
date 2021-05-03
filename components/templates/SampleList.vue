baf59fbb45d659970555db10edb964f6e281a9fa
masayuki-abe <cafenorthfox@gmail.com>
2021-05-03 14:26:24 +0900

    サンプル一覧ページコーディング中

--- components/templates/SampleList.vue
+++ components/templates/SampleList.vue
@@ -2,9 +2,19 @@
   <main>
     <section id="sample-list__box01" class="common-box">
       <h2>Sample List</h2>
-      <p class="common-txt">
-        サンプルのリスト
+      <p v-for="(samples, index) in sampleLists" :key="samples.id" class="common-txt">
+        {{samples.name}}
       </p>
     </section>
   </main>
 </template>
+
+<script>
+import sampleLists from '@/assets/js/sampleList.js'
+
+export default {
+  components: {
+    sampleLists
+  }
+}
+</script>
