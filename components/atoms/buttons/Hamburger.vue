5df0b96f4f9e00a52125057875e4f4bab44d7a33
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-05-10 17:43:07 +0900

    ファイル名変更

---
+++ components/atoms/buttons/Hamburger.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="l-hamburger btn-hamburger"
+    @click="btnMenu"
+  >
+    <span />
+    <span />
+    <span />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    btnMenu () {
+      this.$emit('btnMenu')
+    }
+  }
+}
+</script>
---
+++ components/molecules/lists/NaviList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="l-navilist">
+    <li>
+      <Nuxt-link to="/">
+        Home
+      </Nuxt-link>
+    </li>
+    <li>
+      <Nuxt-link to="/sample">
+        Sample
+      </Nuxt-link>
+    </li>
+    <li>
+      <Nuxt-link to="/about">
+        About us
+      </Nuxt-link>
+    </li>
+    <li>
+      <Nuxt-link to="/contact">
+        Contact
+      </Nuxt-link>
+    </li>
+  </ul>
+</template>
