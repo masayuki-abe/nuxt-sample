9ff700ac6dbcb81f25651202aa010c3521dbf528
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-05-11 16:21:50 +0900

    必要なファイルを作成

---
+++ components/templates/Accordion.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <section id="c-accordion_box01">
+      <div class="c-box">
+        <h2>Accordion</h2>
+        <p class="common-txt">
+          クリックしたら下にしゅるっと伸びていくアコーディオン機能。
+        </p>
+      </div>
+    </section>
+  </main>
+</template>
