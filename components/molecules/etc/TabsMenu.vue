2ea3fddf74d091e600b431b6ed2879cb6c031ea3
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-05-14 17:16:52 +0900

    とりあえずコピペで動かす

--- components/molecules/etc/TabsMenu.vue
+++ components/molecules/etc/TabsMenu.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="c-tabs-menu">
-    <p>tabs menu</p>
+    <ul class="c-tabs-menu_btn">
+      <li v-for="(tab, index) in tabBtns" :key="tab.tabName" class="tabs-btn" :class="[ activetab === (index +1 ) ? 'active' : '' ] " @click="activetab = (index + 1)">
+        {{ tab.tabText }}
+      </li>
+    </ul>
+    <ul class="c-tabs-menu_content">
+      <li v-for="(item, index) in items" :key="item.id" class="c-tabs-menu_block" :class="[ activetab === (index + 1) ? 'show' : '' ]">
+        <div class="c-tabs-menu_inner">
+          <div v-html="item.content" />
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    tabBtns: {
+      type: Array,
+      default: () => []
+    },
+    tabitems: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      activetab: 1
+    }
+  },
+  methods: {
+    console () {
+      console.log(this.tabBtns)
+    }
+  }
+}
+</script>
--- components/templates/TabsMenu.vue
+++ components/templates/TabsMenu.vue
@@ -3,7 +3,70 @@
     <section id="p-tabs-menu_box01">
       <div class="c-box">
         <h2>Tabs Menu</h2>
+        <p class="common-txt">
+          あまり好きじゃない。スマホのときみづらくない？っていつも思う。<br>
+          PCはタブメニューでスマホはアコーディオンとかって、Vue＋1ソースでやるとしたらどうやるんだろう……？
+        </p>
+      </div>
+    </section>
+
+    <section id="p-tabs-menu_box02">
+      <div class="c-box">
+        <h3>配列処理</h3>
+        <p class="common-txt">
+          配列処理で、展開するコンテンツの中身を自由に書けるようにしておけば良いかな、と。
+        </p>
+        <div id="tab">
+          <ul class="tabMenu">
+            <li v-for="(tab,index) in tabs" :key="index" class="tabItem" :class="[ activetab === (index+1) ? 'active' : '' ]" @click="activetab=(index+1)">
+              {{ tab.tabitem }}
+            </li>
+          </ul>
+
+          <div class="tabContent">
+            <div v-for="(item,index) in contents" :key="index" class="tabBlock" :class="[ activetab === (index+1) ? 'show' : '' ]">
+              <div class="tabBlock__inner">
+                <h3 class="tabBlock__title">
+                  {{ item.title }}
+                </h3>
+                <p class="tabBlock__text">
+                  {{ item.text }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </main>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      activetab: 1,
+
+      tabs: [
+        { tabitem: 'tab01' },
+        { tabitem: 'tab02' },
+        { tabitem: 'tab03' }
+      ],
+      contents: [
+        {
+          title: 'コンテンツ01',
+          text: 'テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト'
+        },
+        {
+          title: 'コンテンツ02',
+          text: 'テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト'
+        },
+        {
+          title: 'コンテンツ03',
+          text: 'テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト'
+        }
+      ]
+    }
+  }
+}
+</script>
