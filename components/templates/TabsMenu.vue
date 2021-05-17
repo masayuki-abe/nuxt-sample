c4a8425d86af2f3a26d7c31fc5f95e9023e026a1
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-05-17 09:43:16 +0900

    class名をフロックスルールに変更

--- components/templates/TabsMenu.vue
+++ components/templates/TabsMenu.vue
@@ -14,11 +14,12 @@
       <div class="c-box">
         <h3>配列処理</h3>
         <p class="common-txt">
-          配列処理で、展開するコンテンツの中身を自由に書けるようにしておけば良いかな、と。
+          配列処理で、展開するコンテンツの中身を自由に書けるようにしておけば良いかな、と。<br>
+          なおここでは、メニューとコンテンツのコンポネントをわけて別々にしている。
         </p>
-        <div id="tab">
-          <ul class="tabMenu">
-            <li v-for="(tab,index) in tabs" :key="index" class="tabItem" :class="[ activetab === (index + 1) ? 'active' : '', tab.tabClass ]" @click="activetab=(index+1)">
+        <div id="c-tab">
+          <ul class="c-tab_menu">
+            <li v-for="(tab, index) in tabs" :key="index" class="c-tab_menu--item" :class="[ activetab === (index + 1) ? 'active' : '', tab.tabClass ]" @click="activetab=(index + 1)">
               <TabsMenu
                 :tab-menu-class="tab.tabClass"
                 :tab-menu-name="tab.tabName"
@@ -26,8 +27,8 @@
             </li>
           </ul>
 
-          <div class="tabContent">
-            <div v-for="(item,index) in contents" :key="index" class="tabBlock" :class="[ activetab === (index + s1) ? 'show' : '' ]">
+          <div class="c-tab_content">
+            <div v-for="(item, index) in contents" :key="index" class="c-tab_content--block" :class="[ activetab === (index + 1) ? 'show' : '' ]">
               <TabsContent
                 :tab-content-text="item.text"
               />
@@ -53,9 +54,9 @@ export default {
       activetab: 1,
 
       tabs: [
-        { tabClass: 'tab01', tabName: 'タブ01' },
-        { tabClass: 'tab02', tabName: 'タブ02タブ02' },
-        { tabClass: 'tab03', tabName: 'タブ03' }
+        { tabClass: 'c-tab_menu--item01', tabName: 'タブ01' },
+        { tabClass: 'c-tab_menu--item02', tabName: 'タブ02タブ02' },
+        { tabClass: 'c-tab_menu--item03', tabName: 'タブ03' }
       ],
       contents: [
         {
