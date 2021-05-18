abade122708615a68c5ac8e7e9354206703c8591
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-05-18 16:41:00 +0900

    navilistを配列処理に変更

--- components/molecules/lists/navilist.vue
+++ components/molecules/lists/navilist.vue
@@ -1,24 +1,37 @@
 <template>
   <ul class="l-navilist">
-    <li>
-      <Nuxt-link to="/">
-        Home
-      </Nuxt-link>
-    </li>
-    <li>
-      <Nuxt-link to="/sample">
-        Sample
-      </Nuxt-link>
-    </li>
-    <li>
-      <Nuxt-link to="/about">
-        About us
-      </Nuxt-link>
-    </li>
-    <li>
-      <Nuxt-link to="/contact">
-        Contact
+    <li v-for="navilist in navilists" :key="navilist.path" :class="navilist.path">
+      <Nuxt-link :to="navilist.path">
+        {{ navilist.name }}
       </Nuxt-link>
     </li>
   </ul>
 </template>
+
+<script>
+export default {
+  computed: {
+    navilists () {
+      const navi = [
+        {
+          path: '/',
+          name: 'Home'
+        },
+        {
+          path: '/sample',
+          name: 'Sample'
+        },
+        {
+          path: '/api',
+          name: 'Api'
+        },
+        {
+          path: '/strage',
+          name: 'Strage'
+        }
+      ]
+      return navi
+    }
+  }
+}
+</script>
