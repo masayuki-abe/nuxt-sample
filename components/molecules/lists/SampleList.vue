329d01ec1626e5dea79bc5199c6811f554095a98
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-05-18 15:42:50 +0900

    サンプルページにアコーディオンとタブ切り替えを追加

--- components/molecules/lists/SampleList.vue
+++ components/molecules/lists/SampleList.vue
@@ -88,6 +88,22 @@ export default {
           name: 'Modal Window',
           nameJa: 'モーダルウィンドウ',
           comment: 'ウィンドウ内で、「子ウィンドウ」を展開。課題あり。どう解決するか。'
+        },
+        {
+          id: '3',
+          path: 'accordion',
+          img: '/images/sample/accordion.jpg',
+          name: 'Accordion',
+          nameJa: 'アコーディオン',
+          comment: 'ボタンクリックで下に展開するアコーディオンメニュー（トグルメニュー）。ベタ書きと配列処理の2パタン'
+        },
+        {
+          id: '4',
+          path: 'tabs-menu',
+          img: '/images/sample/tabs-menu.jpg',
+          name: 'Tabs Menu',
+          nameJa: 'タブメニュー',
+          comment: 'タブ切り替え機能。PC・スマホともにタブメニューと、PCではタブ、スマホではアコーディオンの2パタン。どちらも配列処理'
         }
       ]
       return sampleList
@@ -120,6 +136,22 @@ export default {
       .c-modal{
         &_wrap{
           background: url(/images/sample/modal-window.jpg) no-repeat center center;
+          background-size: cover;
+        }
+      }
+    }
+    &.accordion{
+      .c-modal{
+        &_wrap{
+          background: url(/images/sample/accordion.jpg) no-repeat center center;
+          background-size: cover;
+        }
+      }
+    }
+    &.tabs-menu{
+      .c-modal{
+        &_wrap{
+          background: url(/images/sample/tabs-menu.jpg) no-repeat center center;
           background-size: cover;
         }
       }
