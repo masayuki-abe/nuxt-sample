fee262a4078a1e1c6fffb7540b1e1783b9a9fc66
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-06-08 14:20:34 +0900

    サンプルリストの配列をassets内のjsファイルから読み込むように変更

--- components/molecules/lists/SampleList.vue
+++ components/molecules/lists/SampleList.vue
@@ -56,6 +56,8 @@
 </template>
 
 <script>
+import sampleList from 'assets/js/SampleList'
+
 export default {
   data () {
     return {
@@ -66,47 +68,10 @@ export default {
   computed: {
     limitCount () {
       if (this.$route.name === 'index') {
-        return this.samples.slice(0, 3)
+        return sampleList.slice(0, 3)
       } else {
-        return this.samples
+        return sampleList
       }
-    },
-    samples () {
-      const sampleList = [
-        {
-          id: '1',
-          path: 'hamburger-menu',
-          img: '/images/sample/hamburger-menu.jpg',
-          name: 'Hamburger Menu',
-          nameJa: 'ハンバーガーメニュー',
-          comment: '言わずと知れた「三本線」のボタンクリックでメニューの開閉を実行。ヘッダ右上のボタンがそれ。（PC版では非表示）'
-        },
-        {
-          id: '2',
-          path: 'modal-window',
-          img: '/images/sample/modal-window.jpg',
-          name: 'Modal Window',
-          nameJa: 'モーダルウィンドウ',
-          comment: 'ウィンドウ内で、「子ウィンドウ」を展開。課題あり。どう解決するか。'
-        },
-        {
-          id: '3',
-          path: 'accordion',
-          img: '/images/sample/accordion.jpg',
-          name: 'Accordion',
-          nameJa: 'アコーディオン',
-          comment: 'ボタンクリックで下に展開するアコーディオンメニュー（トグルメニュー）。ベタ書きと配列処理の2パタン'
-        },
-        {
-          id: '4',
-          path: 'tabs-menu',
-          img: '/images/sample/tabs-menu.jpg',
-          name: 'Tabs Menu',
-          nameJa: 'タブメニュー',
-          comment: 'タブ切り替え機能。PC・スマホともにタブメニューと、PCではタブ、スマホではアコーディオンの2パタン。どちらも配列処理'
-        }
-      ]
-      return sampleList
     }
   },
   methods: {
