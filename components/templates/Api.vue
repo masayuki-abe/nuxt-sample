670fd84d316c48f807afec839e0d5faec759a506
masayuki.abe <masayuki.abe@adminnoMacBook-Pro.local>
2021-06-09 18:25:40 +0900

    quagga.jsを一部調整

--- components/templates/Api.vue
+++ components/templates/Api.vue
@@ -217,8 +217,16 @@ export default {
           target: document.querySelector('#cameraArea'),
           constraints: { facingMode: 'environment' }
         },
+        locator: {
+          patchSize: 'medium',
+          halfSample: true
+        },
         numOfWorkers: navigator.hardwareConcurrency || 4,
-        decoder: { readers: ['ean_reader', 'ean_8_reader'] }
+        decoder: {
+          readers: ['ean_reader'],
+          multiple: false
+        },
+        locate: true
       }
       this.Quagga.init(config, this.onInit)
     },
@@ -232,10 +240,27 @@ export default {
     },
     onDetected (success) {
       this.code = success.codeResult.code
+      if (this.calc(this.code)) { alert(this.code) }
       // 取得時の画像を表示
       const $resultImg = document.querySelector('.resultImg')
       $resultImg.setAttribute('src', this.Quagga.canvas.dom.image.toDataURL())
       this.Quagga.stop()
+    },
+    calc (isbn) {
+      const arrIsbn = isbn
+        .toString()
+        .split('')
+        .map(num => parseInt(num))
+      let remainder = 0
+      const checkDigit = arrIsbn.pop()
+
+      arrIsbn.forEach((num, index) => {
+        remainder += num * (index % 2 === 0 ? 1 : 3)
+      })
+      remainder %= 10
+      remainder = remainder === 0 ? 0 : 10 - remainder
+
+      return checkDigit === remainder
     },
     onProcessed (result) {
       const drawingCtx = this.Quagga.canvas.ctx.overlay
